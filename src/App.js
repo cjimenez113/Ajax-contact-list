2b71476027bed3132c17b64c8ec5c74395473f26
cjimenez113 <christopherjimenez@Christophers-MacBook-Air.local>
2017-05-11 18:23:09 -0500

    contact-list-Ajax-loaded

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 import ContactList from './ContactList';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
@@ -10,39 +11,18 @@ class App extends Component {
 
     this.state = {
       searchText: '',
-      contacts: [
-        {
-          _id: 1,
-          name: 'Dale Cooper',
-          occupation: 'FBI Agent',
-          avatar: 'https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg'
-        },
-        {
-          _id: 2,
-          name: 'Spike Spiegel',
-          occupation: 'Bounty Hunter',
-          avatar: 'http://vignette4.wikia.nocookie.net/deadliestfiction/images/d/de/Spike_Spiegel_by_aleztron.jpg/revision/latest?cb=20130920231337'
-        },
-        {
-          _id: 3,
-          name: 'Wirt',
-          occupation: 'adventurer',
-          avatar: 'http://66.media.tumblr.com/5ea59634756e3d7c162da2ef80655a39/tumblr_nvasf1WvQ61ufbniio1_400.jpg'
-        },
-        {
-          _id: 4,
-          name: 'Michael Myers',
-          occupation: 'Loving little brother',
-          avatar: 'http://vignette2.wikia.nocookie.net/villains/images/e/e3/MMH.jpg/revision/latest?cb=20150810215746'
-        },
-        {
-          _id: 5,
-          name: 'Dana Scully',
-          occupation: 'FBI Agent',
-          avatar: 'https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg'
-        }
-      ],
+      contacts: [],
     };
+  }
+  componentDidMount() {
+    axios.get('http://localhost:4000/contacts')
+     .then(resp => {
+       this.setState({
+         searchText: this.state.searchText,
+         contacts: resp.data
+       });
+     })
+     .catch(err => console.log(`Error! ${err}`));
   }
   handleSearchBarChange(event) {
     this.setState({
@@ -62,6 +42,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Contact List</h1>
         <SearchBar onChange={this.handleSearchBarChange.bind(this)} value={this.state.searchText} />
         <ContactList contacts={this.getFilteredContacts()} />
       </div>
